545c19fe54a486c2f27dce7e38f5788c8f6b49c9
Noor ul Ain <57191809+nooruluzair@users.noreply.github.com>
2025-01-23 20:49:38 +0500

    Update main.py

--- Class07_Number_Guessing_Game/main.py
+++ Class07_Number_Guessing_Game/main.py
@@ -1,1 +1,18 @@
+import random
+random_number = random.randint(1, 5)                                                      #it will generate a random number between 1 and 5
+no_of_tries = 0                                                                           #Variable to store the number of tries
+
+# print(random_number)                                                                      #Printing the random number generated for testing purpose
+
+
+while True:
+    user_entry = int(input("Enter a number between 1 and 5: "))                        #Asking the user to enter a number between 1 and 100
+    no_of_tries += 1                                                                      #Incrementing the number of tries by 1
+    if user_entry == random_number:                                                       #Checking if the user entered number is equal to the random number                                  
+        print("Wohoo!, You guessed the correct number in", no_of_tries, "tries")
+        break
+    else:
+        print("That's incorrect. Please Try again.")                                                 #Printing a message to try again
+        continue
+    
 
